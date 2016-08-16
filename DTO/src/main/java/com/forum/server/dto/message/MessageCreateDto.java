40547d6608229e0f946811d473429e97920df630
alchemist3661 <sashasalin366@gmail.com>
2016-08-16 16:37:05 +0400

    m

--- DTO/src/main/java/com/forum/server/dto/message/MessageCreateDto.java
+++ DTO/src/main/java/com/forum/server/dto/message/MessageCreateDto.java
@@ -1,8 +1,10 @@
 package com.forum.server.dto.message;
 
+import com.forum.server.dto.Data;
+
 /**
  * Created by aisalin on 15.08.16.
  */
-public class MessageCreateDto {
+public class MessageCreateDto implements Data {
     // TODO: 15.08.16 Добавить поля
 }
--- Web/src/main/java/com/forum/server/controller/MessagesController.java
+++ Web/src/main/java/com/forum/server/controller/MessagesController.java
@@ -3,12 +3,13 @@ package com.forum.server.controller;
 import com.forum.server.dto.message.MessageCreateDto;
 import com.forum.server.dto.message.MessageDto;
 import com.forum.server.dto.response.QueryResultDto;
-import com.forum.server.services.interfaces.MessagesService;
+import com.forum.server.services.interfaces.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import static com.forum.server.utils.ResponseBuilder.buildResponseDelete;
 import static com.forum.server.utils.ResponseBuilder.buildResponsePostAndPut;
 import static org.springframework.web.bind.annotation.RequestMethod.*;
 
@@ -21,16 +22,40 @@ import static org.springframework.web.bind.annotation.RequestMethod.*;
 @Controller
 public class MessagesController {
     @Autowired
-    private MessagesService messagesService;
+    private MessageService messageService;
 
     @RequestMapping(value = "/themes/{theme-id}", method = POST)
-    public ResponseEntity<QueryResultDto> createMessage(@RequestHeader(name = "Auth-Token") String token,
-                                                        @PathVariable("theme-id") int themeId,
-                                                        @RequestBody MessageCreateDto messageCreateDto) {
-        MessageDto message = messagesService.createMessage(token, themeId, messageCreateDto);
+    public ResponseEntity<QueryResultDto> createMessage(@PathVariable("theme-id") int themeId,
+                                                        @RequestBody MessageCreateDto messageCreateDto,
+                                                        @RequestHeader(name = "Auth-Token") String token) {
+        MessageDto message = messageService.createMessage(token, themeId, messageCreateDto);
         return buildResponsePostAndPut(message);
     }
 
+    @RequestMapping(value = "/themes/{theme-id}/{message-id}", method = PUT)
+    public ResponseEntity<QueryResultDto> updateMessage(@PathVariable("theme-id") int themeId,
+                                                        @PathVariable("message-id") int messageId,
+                                                        @RequestBody MessageDto message,
+                                                        @RequestHeader(name = "Auth-Token") String token) {
+        MessageDto updatedMessage = messageService.updateMessage(token, themeId, messageId, message);
+        return buildResponsePostAndPut(updatedMessage);
+    }
+
+    @RequestMapping(value = "/themes/{theme-id}/{message-id}/rating", method = PUT)
+    public ResponseEntity<QueryResultDto> updateMessageRating(@PathVariable("theme-id") int themeId,
+                                                        @PathVariable("message-id") int messageId,
+                                                        @RequestParam("grade") boolean grade) {
+        MessageDto updatedMessage = messageService.updateMessageRating(themeId, messageId, grade);
+        return buildResponsePostAndPut(updatedMessage);
+    }
+
+    @RequestMapping(value = "/themes/{theme-id}/{message-id}", method = DELETE)
+    public ResponseEntity<QueryResultDto> deleteTheme(@PathVariable("theme-id") int themeId,
+                                                      @PathVariable("message-id") int messageId,
+                                                      @RequestHeader(name = "Auth-Token") String token) {
+        messageService.deleteMessage(token, themeId, messageId);
+        return buildResponseDelete();
+    }
 }
 
 
