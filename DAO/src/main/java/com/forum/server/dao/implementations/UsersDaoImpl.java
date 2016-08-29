9b3aae0967efb3a0cc5a84d8e4ad97089cb4f30a
Rafteri <rafteri98@gmail.com>
2016-08-29 12:49:42 +0300

    impl usersDao

--- DAO/src/main/java/com/forum/server/dao/implementations/UsersDaoImpl.java
+++ DAO/src/main/java/com/forum/server/dao/implementations/UsersDaoImpl.java
@@ -19,26 +19,48 @@ public class UsersDaoImpl implements UsersDao {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    //TODO Реализовать методы
     private static final String SQL_IS_EXISTS_NICKNAME = "SELECT CASE WHEN EXISTS(SELECT user_id FROM short_user WHERE nick_name = ?)THEN TRUE ELSE FALSE END ;";
-    private static final String SQL_IS_EXISTS_MAIL = "SELECT CASE WHEN EXISTS(SELECT user_id FROM user_info WHERE nick_name = ?)THEN TRUE ELSE FALSE END ;";
-    private static final String SQL_GET_HASH_BY_NICKNAME = "SELECT CASE WHEN EXISTS(SELECT user_id FROM user_info WHERE nick_name = ?)THEN TRUE ELSE FALSE END ;";
+    private static final String SQL_IS_EXISTS_MAIL = "SELECT CASE WHEN EXISTS(SELECT user_id FROM user_info WHERE mail = ?)THEN TRUE ELSE FALSE END ;";
+    private static final String SQL_GET_HASH_BY_NICKNAME = "SELECT pass_hash FROM user_info WHERE user_id = (SELECT user_id FROM short_user WHERE nick_name = ?);";
+    private static final String SQL_GET_HASH_BY_MAIL = "SELECT pass_hash FROM user_info WHERE mail = ?;";
+    private static final String SQL_FIND_BY_TOKEN = "SELECT * FROM user_info INNER JOIN short_user ON user_info.user_id = short_user.user_id WHERE short_user.user_id = (SELECT user_id FROM auth WHERE token = ?);";
+    private static final String SQL_GET_ID_BY_MAIL = "SELECT user_id FROM user_info WHERE mail = ?;";
+    private static final String SQL_GET_ID_BY_NICKNAME = "SELECT user_id FROM short_user WHERE nick_name = ?;";
 
 
-    public User findByToken(String token) {
-        return null;
+    private RowMapper<User> userRowMapper() {
+        return (rs, i) -> new User.Builder()
+                .UserId(rs.getInt("user_id"))
+                .NickName(rs.getString("nick_name"))
+                .Rating(rs.getInt("rating"))
+                .Avatar(rs.getString("avatar"))
+                .IsOnline(rs.getBoolean("is_online"))
+                .Mail(rs.getString("mail"))
+                .DateOfBirth(rs.getInt("birth_date"))
+                .Info(rs.getString("info"))
+                .Rights(rs.getString("rights"))
+                .RegistrationTime(rs.getInt("registration_time"))
+                .LastSession(rs.getInt("last_session"))
+                .MessagesCount(rs.getInt("messages_count"))
+                .ThemesCount(rs.getInt("themes_count"))
+                .HashPassword(rs.getString("pass_hash"))
+                .build();
     }
 
-    public boolean isExixstsMail(String identifier) {
-        return false;
+    public User findByToken(String token) {
+        return jdbcTemplate.queryForObject(SQL_FIND_BY_TOKEN, userRowMapper(), token);
+    }
+
+    public boolean isExistsMail(String identifier) {
+        return jdbcTemplate.queryForObject(SQL_IS_EXISTS_MAIL, boolean.class, identifier);
     }
 
     public String getHashByMail(String identifier) {
-        return null;
+        return jdbcTemplate.queryForObject(SQL_GET_HASH_BY_MAIL, String.class, identifier);
     }
 
-    public Integer findIdByMail(String identifier) {
-        return null;
+    public Integer getIdByMail(String identifier) {
+        return jdbcTemplate.queryForObject(SQL_GET_ID_BY_MAIL, Integer.class, identifier);
     }
 
     public boolean isExistsNickName(String identifier) {
@@ -46,11 +68,11 @@ public class UsersDaoImpl implements UsersDao {
     }
 
     public String getHashByNickName(String identifier) {
-        return null;
+        return jdbcTemplate.queryForObject(SQL_GET_HASH_BY_NICKNAME, String.class, identifier);
     }
 
-    public Integer findIdByNickName(String identifier) {
-        return null;
+    public Integer getIdByNickName(String identifier) {
+        return jdbcTemplate.queryForObject(SQL_GET_ID_BY_NICKNAME, Integer.class, identifier);
     }
 
     public void save(User user) {
--- DAO/src/test/java/com/forum/server/dao/UsersDaoImplTest.java
+++ DAO/src/test/java/com/forum/server/dao/UsersDaoImplTest.java
@@ -8,10 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 /**
- * 28.08.16
+ * 29.08.16
  *
  * @author Dinar Rafikov (First Software Engineering Platform)
  * @version 1.0
@@ -20,11 +20,53 @@ import static org.junit.Assert.assertTrue;
 @ContextConfiguration(classes = PersistenceConfig.class)
 public class UsersDaoImplTest {
 
+    private static final String MAIL = "rafteri98@gmail.com";
+    private static final String TOKEN = "token";
+    private static final String NICKNAME = "Din";
+    private static final String PASS_HASH = "lol";
+    private static final int ID = 1;
+
     @Autowired
     private UsersDao usersDao;
 
     @Test
-    public void testIsExistsNickname() throws Exception {
-        assertTrue(usersDao.isExistsNickName("Din"));
+    public void findByToken() throws Exception {
+        assertNotNull(usersDao.findByToken(TOKEN));
     }
+
+    @Test
+    public void isExistsMail() throws Exception {
+        assertTrue(usersDao.isExistsMail(MAIL));
+    }
+
+    @Test
+    public void getHashByMail() throws Exception {
+        assertEquals(PASS_HASH, usersDao.getHashByMail(MAIL));
+    }
+
+    @Test
+    public void getIdByMail() throws Exception {
+        assertTrue(ID == usersDao.getIdByMail(MAIL));
+    }
+
+    @Test
+    public void isExistsNickName() throws Exception {
+        assertTrue(usersDao.isExistsNickName(NICKNAME));
+    }
+
+    @Test
+    public void getHashByNickName() throws Exception {
+        assertEquals(PASS_HASH, usersDao.getHashByNickName(NICKNAME));
+    }
+
+    @Test
+    public void getIdByNickName() throws Exception {
+        assertTrue(ID == usersDao.getIdByNickName(NICKNAME));
+    }
+
+    @Test
+    public void save() throws Exception {
+
+    }
+
 }
